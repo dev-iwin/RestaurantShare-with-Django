d3d11f0774639a21348294866c040f0a6dabc9db
dev-iwin <iwinoriwin@gmail.com>
2021-01-29 04:41:20 +0900

    카테고리 Create 기능 구현

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -10,10 +12,17 @@ def restaurantDetail(request):
     # return HttpResponse("restaurantDetail")
     return render(request, 'shareRes/restaurantDetail.html')
 
-def restaurantCreat(request):
-    # return HttpResponse("restaurantCreat")
-    return render(request, 'shareRes/restaurantCreat.html')
+def restaurantCreate(request):
+    # return HttpResponse("restaurantCreate")
+    return render(request, 'shareRes/restaurantCreate.html')
 
 def categoryCreate(request):
     # return HttpResponse("categoryCreate")
     return render(request, 'shareRes/categoryCreate.html')
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("여기서 category Creat 기능을 구현할 거야.")
